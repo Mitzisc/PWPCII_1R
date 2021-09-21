4e858c813690bae6ff4e3e64762bf4eacd8cea75
Mitzi SSanchez <nayesc98@gmail.com>
2021-09-20 22:49:29 -0500

    proyecto funcionando

---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', author:'Mitzi Sanchez',appName:'web' });
+
+
+});
+
+/*Agregando una nueva ruta*/ 
+router.get('/greeting',function(req, res, next){
+res.status(200).json({message: 'prueba dos'})
+});
+
+
+/*Reto*/
+
+router.get('/new',function(req, res, next){
+  res.status(200).json({message: 'Reto cumplido'})
+});
+module.exports = router;
+ 
