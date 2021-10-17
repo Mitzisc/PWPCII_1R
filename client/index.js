7d321b8b375b9f8922f95291c1f13ac4d3fed341
Mitzi SSanchez <nayesc98@gmail.com>
2021-10-16 23:35:18 -0500

    video 27

--- client/index.js
+++ client/index.js
@@ -1,27 +1,28 @@
-import './stylesheets/style.css'
-import './stylesheets/mystyles.css'
+/* eslint-disable linebreak-style */
+/* eslint-disable no-alert */
+/* eslint-disable no-console */
+import './stylesheets/style.css';
+import './stylesheets/mystyles.css';
 
-console.log("webpack working .!!");
-//Default parameters ES6/2015
-let show=(m = "hola") => {
-    alert(m)
+console.log('webpack Working!!!');
+// Default parameters Es6/2015
+const show = (m = 'Hot module represent working') => {
+  alert(m);
 };
 
 show();
 
-function resolveAfter2Seconds(){
-    return  new Promise (resolve =>{
-        setTimeout(() =>{
-            resolve ('resolved')
-        },2000);
-    });
+function resolveAfter2Seconds() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('resolved');
+    }, 2000);
+  });
 }
-
-
-async function asyncCall(){
-    console.log("calling an async function");
-    const result = await resolveAfter2Seconds();
-    console.log(result);
+async function asyncCall() {
+  console.log('Calling an async function');
+  const result = await resolveAfter2Seconds();
+  console.log(result);
 }
 
 asyncCall();
--- server/app.js
+++ server/app.js
@@ -1,4 +1,14 @@
-
+/* eslint-disable no-unused-vars */
+/* eslint-disable func-names */
+/* eslint-disable quotes */
+/* eslint-disable no-console */
+/* eslint-disable indent */
+/* eslint-disable comma-spacing */
+/* eslint-disable no-var */
+/* eslint-disable spaced-comment */
+/* eslint-disable import/no-duplicates */
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable import/no-unresolved */
 import createError from 'http-errors';
 
 import express from 'express';
@@ -9,11 +19,11 @@ import cookieParser from 'cookie-parser';
 
 import logger from 'morgan';
 
-
 import indexRouter from '@s-routes/index';
 
 import usersRouter from '@s-routes/users';
 
+// eslint-disable-next-line spaced-comment
 //importar modulos de webpack
 import webpack from 'webpack';
 import WebpackDevMiddleware from 'webpack-dev-middleware';
@@ -21,30 +31,39 @@ import WebpackHotMiddleware from 'webpack-hot-middleware';
 import WebpackConfig from '../webpack.dev.config';
 import webpackDevConfig from '../webpack.dev.config';
 
+// eslint-disable-next-line spaced-comment
 //consultar modo en que se ejecuta la aplicacion
 const env = process.env.NODE_ENV || 'developement';
 
 //creacion aplicacion express
-var app = express();
+const app = express();
 
 //verficiar modo ejecucion de la aplicacion
-if(env === 'development'){
+// eslint-disable-next-line space-before-blocks
+if (env === 'development') {
   console.log('> Excecuting in Development Mode: Webpack hot Reloading');
   //ruta del Hot module replasmen
   //reload=true: habilita recarga fronted al tener cambios en codigo fuente del fronted
   //timeout=1000: Tiempo espera recarga
-  WebpackConfig.entry = ['Webpack-hot-middleware/client?reload=true&timeout=1000', WebpackConfig.entry];
+  WebpackConfig.entry = [
+    'Webpack-hot-middleware/client?reload=true&timeout=1000',
+    WebpackConfig.entry,
+  ];
   //Agregar plugin
   WebpackConfig.plugins.push(new webpack.HotModuleReplacementPlugin());
   //compilador
   const compiler = webpack(WebpackConfig);
   //Agregando middleware a cadena
-  app.use(WebpackDevMiddleware(compiler,{
-  publicPath: webpackDevConfig.output.publicPath
-}));
+  // eslint-disable-next-line object-curly-spacing
+  app.use(
+    WebpackDevMiddleware(compiler, {
+      publicPath: webpackDevConfig.output.publicPath,
+    }),
+  );
 // webpack hot middleware
   app.use(WebpackHotMiddleware(compiler));
-}else{
+  // eslint-disable-next-line keyword-spacing
+} else {
   console.log('> Excecuting in Production Mode... ');
 }
 
@@ -62,12 +81,17 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+// eslint-disable-next-line prefer-arrow-callback
+app.use( (req, res, next) => {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+// eslint-disable-next-line prefer-arrow-callback
+// eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line func-names
+// eslint-disable-next-line prefer-arrow-callback
+app.use( (err, req, res) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -77,4 +101,5 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+// eslint-disable-next-line eol-last
 module.exports = app;
